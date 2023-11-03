a1b17ca56eb0e1d92c08ae868dc6e8906b1b2531
KarlEaubed <118594840+KarlEaubed@users.noreply.github.com>
2023-11-02 22:39:10 -0400

    Premye commit

--- pratique.py
+++ pratique.py
@@ -1,24 +1,24 @@
 
-# def get_miniskil():
-#     text = input("Ekri yon bagay: ")
-#     miniskil_text = text.lower()
-#     print(miniskil_text)
+def get_miniskil():
+    text = input("Ekri yon bagay: ")
+    miniskil_text = text.lower()
+    print(miniskil_text)
 
-# get_miniskil()
+get_miniskil()
 
-# def get_split():
-#     text = input("Ekri yon fraz: ")
-#     split_text = text.split()
-#     print(split_text)
+def get_split():
+    text = input("Ekri yon fraz: ")
+    split_text = text.split()
+    print(split_text)
 
-# get_split()
+get_split()
 
-# def get_premyemo_majiskil():
-#     text = input("Ekri yon fraz: ")
-#     title_text = text.title()
-#     print(title_text)
+def get_premyemo_majiskil():
+    text = input("Ekri yon fraz: ")
+    title_text = text.title()
+    print(title_text)
 
-# get_premyemo_majiskil()
+get_premyemo_majiskil()
 
 def get_inisyal():
     text = "Nan yon chenn karaktè, rekipere premye lèt chak mo."
